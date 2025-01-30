9d35cdfff00a760e88b132a37cf29444c4ea3224
shady yasser <shadyyasser172003@gmail.com>
2025-01-30 22:34:42 +0200

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(Hello, Jenkins with Gitea!);
+        System.out.println("Hello, Jenkins Pipeline!");
     }
 }
-
